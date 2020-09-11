f7731c1f23321995d5b853a39d15e8bd527b527c
Dooster15 <dooster12@hotmail.co.uk>
2020-09-11 15:44:46 +0100

    navbar

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -6,14 +6,17 @@ import octoprint.plugin
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin):
-    def on_after_startup(self):
-        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+def on_after_startup(self):
+    self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
 
-    def get_settings_defaults(self):
-        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+def get_settings_defaults(self):
+    return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
-    def get_template_vars(self):
-        return dict(url=self._settings.get(["url"]))
+def get_template_configs(self):
+    return [
+        dict(type="navbar", custom_bindings=False),
+        dict(type="settings", custom_bindings=False)
+    ]
 
 __plugin_name__ = "Hello World"
 __plugin_pythoncompat__ = ">=2.7,<4"
