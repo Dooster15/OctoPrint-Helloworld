1c9a973780c1192382c1adfc4dd777da7058a258
Dooster15 <dooster12@hotmail.co.uk>
2020-09-11 16:45:27 +0100

    no message

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -6,17 +6,14 @@ import octoprint.plugin
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin):
-def on_after_startup(self):
-    self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+    def on_after_startup(self):
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
 
-def get_settings_defaults(self):
-    return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
-def get_template_configs(self):
-    return [
-        dict(type="navbar", custom_bindings=False),
-        dict(type="settings", custom_bindings=False)
-    ]
+    def get_template_vars(self):
+        return dict(url=self._settings.get(["url"]))
 
 __plugin_name__ = "Hello World"
 __plugin_pythoncompat__ = ">=2.7,<4"
